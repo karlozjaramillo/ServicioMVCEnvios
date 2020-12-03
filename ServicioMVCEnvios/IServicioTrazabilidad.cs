fd4ded85119778a1389af963ff6777c9157be195
Karloz <karloz.jaramillo@hotmail.com>
2020-12-02 21:57:40 -0500

    Se agrega ServicioTrazabilidad

---
+++ ServicioMVCEnvios/IServicioTrazabilidad.cs
@@ -0,0 +1,30 @@
+﻿using ServicioMVCEnvios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServicioMVCEnvios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicioTrazabilidad" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicioTrazabilidad
+    {
+        [OperationContract]
+        void AgregarTrazabilidad(Trazabilidad trazabilidad);
+
+        [OperationContract]
+        List<Trazabilidad> ListarTrazabilidades();
+
+        [OperationContract]
+        void EliminarTrazabilidades(long id);
+
+        [OperationContract]
+        void EditarTrazabilidades(Trazabilidad trazabilidad);
+
+        [OperationContract]
+        Trazabilidad BuscarTrazabilidad(long id);
+    }
+}
---
+++ ServicioMVCEnvios/ServicioTrazabilidad.svc.cs
@@ -0,0 +1,46 @@
+﻿using ServicioMVCEnvios.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServicioMVCEnvios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioTrazabilidad" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ServicioTrazabilidad.svc o ServicioTrazabilidad.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ServicioTrazabilidad : IServicioTrazabilidad
+    {
+        private MVCEnviosEntities db = new MVCEnviosEntities();
+        public void AgregarTrazabilidad(Trazabilidad trazabilidad)
+        {
+            db.Trazabilidad.Add(trazabilidad);
+            db.SaveChanges();
+        }
+
+        public Trazabilidad BuscarTrazabilidad(long id)
+        {
+            return db.Trazabilidad.Find(id);
+        }
+
+        public void EditarTrazabilidades(Trazabilidad trazabilidad)
+        {
+            db.Entry(trazabilidad).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void EliminarTrazabilidades(long id)
+        {
+            Trazabilidad trazabilidad = db.Trazabilidad.Find(id);
+            db.Trazabilidad.Remove(trazabilidad);
+            db.SaveChanges();
+        }
+
+        public List<Trazabilidad> ListarTrazabilidades()
+        {
+            return db.Trazabilidad.ToList();
+        }
+    }
+}
