7251f363249e6636f20a03fd2f769da6b42833c9
Karloz <karloz.jaramillo@hotmail.com>
2020-12-02 20:59:41 -0500

    Se crea ServicioCliente

---
+++ ServicioMVCEnvios/IServicioCliente.cs
@@ -0,0 +1,30 @@
+﻿using ServicioMVCEnvios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServicioMVCEnvios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicioCliente" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicioCliente
+    {
+        [OperationContract]
+        void AgregarCliente(Cliente cliente);
+
+        [OperationContract]
+        List<Cliente> ListarClientes();
+
+        [OperationContract]
+        void EliminarClientes(long id);
+
+        [OperationContract]
+        void EditarClientes(Cliente cliente);
+
+        [OperationContract]
+        Cliente BuscarCliente(long id);
+    }
+}
---
+++ ServicioMVCEnvios/ServicioCliente.svc.cs
@@ -0,0 +1,46 @@
+﻿using ServicioMVCEnvios.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServicioMVCEnvios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioCliente" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ServicioCliente.svc o ServicioCliente.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ServicioCliente : IServicioCliente
+    {
+        private MVCEnviosEntities db = new MVCEnviosEntities();
+        public void AgregarCliente(Cliente cliente)
+        {
+            db.Cliente.Add(cliente);
+            db.SaveChanges();
+        }
+
+        public Cliente BuscarCliente(long id)
+        {
+            return db.Cliente.Find(id);
+        }
+
+        public void EditarClientes(Cliente cliente)
+        {
+            db.Entry(cliente).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void EliminarClientes(long id)
+        {
+            Cliente cliente = db.Cliente.Find(id);
+            db.Cliente.Remove(cliente);
+            db.SaveChanges();
+        }
+
+        public List<Cliente> ListarClientes()
+        {
+            return db.Cliente.ToList();
+        }
+    }
+}
