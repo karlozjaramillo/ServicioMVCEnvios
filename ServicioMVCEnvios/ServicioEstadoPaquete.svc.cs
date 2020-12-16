2537565b769ceb22b2006dd8b0f81a4a5b670223
Karloz <karloz.jaramillo@hotmail.com>
2020-12-16 18:53:09 -0500

    Se crea el servicio ServicioEstadoPaquete

---
+++ ServicioMVCEnvios/ServicioEstadoPaquete.svc.cs
@@ -0,0 +1,21 @@
+﻿using ServicioMVCEnvios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServicioMVCEnvios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioEstadoPaquete" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ServicioEstadoPaquete.svc o ServicioEstadoPaquete.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ServicioEstadoPaquete : IServicioEstadoPaquete
+    {
+        private MVCEnviosEntities db = new MVCEnviosEntities();
+        public List<EstadoPaquete> ListarEstadosPaquete()
+        {
+            return db.EstadoPaquete.ToList();
+        }
+    }
+}
